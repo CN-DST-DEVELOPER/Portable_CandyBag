155883a392cdee5f8a7a589f783ef6a531c802be
LSDust <2250919876@qq.com>
2022-09-15 15:19:22 +0800

    种子袋修改
    种子袋修改逻辑同糖果袋

--- modmain.lua
+++ modmain.lua
@@ -14,6 +14,7 @@ local function ondropped(inst)
     end
 end
 
+----------------------------------------------糖果袋----------------------------------------------
 local function candybag_item_fn(container, item, slot)
     local ret1 = GetModConfigData("冬季盛宴零食") and item:HasTag("wintersfeastfood")
     local ret2 = GetModConfigData("冬季盛宴装饰") and item:HasTag("winter_ornament")
@@ -28,7 +29,7 @@ local function candybag_item_fn(container, item, slot)
         ret3
 end
 
---自定义糖果袋属性
+--糖果袋属性
 local candybag_data = {
     widget = {
         slotpos = {},
@@ -49,7 +50,7 @@ end
 candybag_data.itemtestfn = candybag_item_fn
 candybag_data.priorityfn = candybag_data.itemtestfn
 
---糖果袋扩展逻辑
+--糖果袋修改逻辑
 local function candybag_fn(inst)
     if not GLOBAL.TheWorld.ismastersim then
         return
@@ -71,6 +72,55 @@ end
 
 AddPrefabPostInit("candybag", candybag_fn)
 
+----------------------------------------------种子袋----------------------------------------------
+local function seedpouch_item_fn(container, item, slot)
+    return item.prefab == "seeds" or string.match(item.prefab, "_seeds") or item:HasTag("treeseed")
+end
+
+--种子袋属性
+seedpouch_data = {
+    widget = {
+        slotpos = {},
+        animbank = "ui_krampusbag_2x8",
+        animbuild = "ui_krampusbag_2x8",
+        pos = Vector3(-140, -120, 0)
+    },
+    issidewidget = true,
+    type = "medal_box",
+    openlimit = 1
+}
+
+for y = 0, 6 do
+    table.insert(seedpouch_data.widget.slotpos, Vector3(-162, -75 * y + 240, 0))
+    table.insert(seedpouch_data.widget.slotpos, Vector3(-162 + 75, -75 * y + 240, 0))
+end
+
+seedpouch_data.itemtestfn = seedpouch_item_fn
+seedpouch_data.priorityfn = seedpouch_data.itemtestfn
+
+--种子袋修改逻辑
+local function seedpouch_fn(inst)
+    if not GLOBAL.TheWorld.ismastersim then
+        return
+    end
+
+    if inst.components.container then
+        inst.components.container:WidgetSetup("seedpouch", seedpouch_data)
+    end
+
+    if inst.components.inventoryitem then
+        inst.components.inventoryitem.cangoincontainer = true
+        inst.components.inventoryitem:SetOnDroppedFn(ondropped)
+        if inst.components.equippable then
+            -- inst:AddTag("portable_bag")
+            inst:RemoveComponent("equippable")
+        end
+    end
+end
+
+AddPrefabPostInit("seedpouch", seedpouch_fn)
+
+----------------------------------------------优先级----------------------------------------------
 --参考能力勋章(优先入盒)
 local function inventory_fn(self)
     local oldGiveItem = self.GiveItem
@@ -113,19 +163,21 @@ local function inventory_fn(self)
             --是否符合模组容器条件，选择模组容器
             if candybag_item_fn(nil, inst, nil) then
                 portable_bag_name = "candybag"
+            elseif seedpouch_item_fn(nil, inst, nil) then
+                portable_bag_name = "seedpouch"
             end
         end
-        --根据盒子名搜索玩家身上以及背包中合适的容器
+        --根据容器名搜索玩家身上以及背包中合适的容器
         if portable_bag_name then
-            local medal_boxs =
+            local portable_bags =
                 self:FindItems(
                 function(item)
                     return item.prefab == portable_bag_name and item.components.container and
                         item.components.container:IsFull() == false
                 end
             )
-            if #medal_boxs > 0 then
-                for k, v in ipairs(medal_boxs) do
+            if #portable_bags > 0 then
+                for k, v in ipairs(portable_bags) do
                     portable_bag_container = v.components.container
                     break
                 end
