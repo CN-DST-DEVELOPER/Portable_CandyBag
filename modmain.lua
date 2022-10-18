6309ee6cc8dcd70d0ffd3d28b0f770965bbd1c22
LSDust <2250919876@qq.com>
2022-10-18 15:05:24 +0800

    背包融合布局适应
    背包UI位置修改

--- modmain.lua
+++ modmain.lua
@@ -7,13 +7,23 @@ GLOBAL.setmetatable(
     }
 )
 
+local containers = require("containers")
+local params = containers.params
+
 --丢下
-local function ondropped(inst)
-    if inst.components.container then
-        inst.components.container:Close()
-    end
+-- local function ondropped(inst)
+--     if inst.components.container then
+--         inst.components.container:Close()
+--     end
+-- end
+
+local function onopen(inst)
+    inst.SoundEmitter:PlaySound("dontstarve/movement/foley/backpack")
 end
 
+local function onclose(inst)
+    inst.SoundEmitter:PlaySound("dontstarve/movement/foley/backpack")
+end
 ----------------------------------------------糖果袋----------------------------------------------
 local function candybag_item_fn(container, item, slot)
     local ret1 = GetModConfigData("winter_food") and item:HasTag("wintersfeastfood")
@@ -32,36 +42,36 @@ local function candybag_item_fn(container, item, slot)
 end
 
 --糖果袋属性
-local candybag_data = {
+params.candybag = {
     widget = {
         slotpos = {},
         animbank = "ui_krampusbag_2x8",
         animbuild = "ui_krampusbag_2x8",
-        pos = Vector3(-140, -120, 0)
+        pos = Vector3(300, -120, 0)
     },
-    issidewidget = true,
-    type = "medal_box",
-    openlimit = 1
+    type = "cooker"
 }
 
 for y = 0, 6 do
-    table.insert(candybag_data.widget.slotpos, Vector3(-162, -75 * y + 240, 0))
-    table.insert(candybag_data.widget.slotpos, Vector3(-162 + 75, -75 * y + 240, 0))
+    table.insert(params.candybag.widget.slotpos, Vector3(-162, -75 * y + 240, 0))
+    table.insert(params.candybag.widget.slotpos, Vector3(-162 + 75, -75 * y + 240, 0))
 end
 
-candybag_data.itemtestfn = candybag_item_fn
-candybag_data.priorityfn = candybag_data.itemtestfn
+params.candybag.itemtestfn = candybag_item_fn
+params.candybag.priorityfn = params.candybag.itemtestfn
 
 --糖果袋修改逻辑
 local function candybag_fn(inst)
     if TheNet:GetIsServer() then
         if inst.components.container then
-            inst.components.container:WidgetSetup("candybag", candybag_data)
+            inst.components.container:WidgetSetup("candybag")
+            inst.components.container.onopenfn = onopen
+            inst.components.container.onclosefn = onclose
         end
 
         if inst.components.inventoryitem then
             inst.components.inventoryitem.cangoincontainer = true
-            inst.components.inventoryitem:SetOnDroppedFn(ondropped)
+            -- inst.components.inventoryitem:SetOnDroppedFn(ondropped)
             if inst.components.equippable then
                 -- inst:AddTag("portable_bag")
                 inst:RemoveComponent("equippable")
@@ -70,7 +80,7 @@ local function candybag_fn(inst)
     elseif TheNet:GetIsClient() then
         inst.OnEntityReplicated = function(inst)
             if inst.replica.container then
-                inst.replica.container:WidgetSetup("candybag", candybag_data)
+                inst.replica.container:WidgetSetup("candybag")
             end
         end
     end
@@ -84,36 +94,36 @@ local function seedpouch_item_fn(container, item, slot)
 end
 
 --种子袋属性
-local seedpouch_data = {
+params.seedpouch = {
     widget = {
         slotpos = {},
         animbank = "ui_krampusbag_2x8",
         animbuild = "ui_krampusbag_2x8",
-        pos = Vector3(-140, -120, 0)
+        pos = Vector3(300, -120, 0)
     },
-    issidewidget = true,
-    type = "medal_box",
-    openlimit = 1
+    type = "cooker"
 }
 
 for y = 0, 6 do
-    table.insert(seedpouch_data.widget.slotpos, Vector3(-162, -75 * y + 240, 0))
-    table.insert(seedpouch_data.widget.slotpos, Vector3(-162 + 75, -75 * y + 240, 0))
+    table.insert(params.seedpouch.widget.slotpos, Vector3(-162, -75 * y + 240, 0))
+    table.insert(params.seedpouch.widget.slotpos, Vector3(-162 + 75, -75 * y + 240, 0))
 end
 
-seedpouch_data.itemtestfn = seedpouch_item_fn
-seedpouch_data.priorityfn = seedpouch_data.itemtestfn
+params.seedpouch.itemtestfn = seedpouch_item_fn
+params.seedpouch.priorityfn = params.seedpouch.itemtestfn
 
 --种子袋修改逻辑
 local function seedpouch_fn(inst)
     if TheNet:GetIsServer() then
         if inst.components.container then
-            inst.components.container:WidgetSetup("seedpouch", seedpouch_data)
+            inst.components.container:WidgetSetup("seedpouch")
+            inst.components.container.onopenfn = onopen
+            inst.components.container.onclosefn = onclose
         end
 
         if inst.components.inventoryitem then
             inst.components.inventoryitem.cangoincontainer = true
-            inst.components.inventoryitem:SetOnDroppedFn(ondropped)
+            -- inst.components.inventoryitem:SetOnDroppedFn(ondropped)
             if inst.components.equippable then
                 -- inst:AddTag("portable_bag")
                 inst:RemoveComponent("equippable")
@@ -122,7 +132,7 @@ local function seedpouch_fn(inst)
     elseif TheNet:GetIsClient() then
         inst.OnEntityReplicated = function(inst)
             if inst.replica.container then
-                inst.replica.container:WidgetSetup("seedpouch", seedpouch_data)
+                inst.replica.container:WidgetSetup("seedpouch")
             end
         end
     end
