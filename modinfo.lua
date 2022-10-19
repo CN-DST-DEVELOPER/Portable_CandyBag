b4d43c6f2a2167e7803ee3d1a2e0c04459f896a8
LSDust <2250919876@qq.com>
2022-10-19 15:29:12 +0800

    1.0.5发布版本
    暂时舍弃了靠边UI
    暂时舍弃了拾取优先级

--- modinfo.lua
+++ modinfo.lua
@@ -12,11 +12,11 @@ You may obtain a copy of the License at
 name = "便携糖果袋"
 author = "辰暮"
 description = [[
-为方便节日物品的拾取、储存、使用, 糖果袋不再可装备, 可以放在背包箱子等物品栏
+为方便节日物品的拾取、储存、使用, 糖果袋不再可装备, 可以放在背包、箱子等通用物品栏
 可设置是否能装入冬季盛宴零食、冬季盛宴装饰、万圣节药剂、糖豆
-种子袋已同样成为便携袋子
+可设置种子袋也变为便携袋子
 ]]
-version = "1.0.4"
+version = "1.0.5"
 dst_compatible = true
 forge_compatible = false
 gorge_compatible = false
--- modmain.lua
+++ modmain.lua
@@ -7,7 +7,6 @@ GLOBAL.setmetatable(
     }
 )
 
---丢下
 local function ondropped(inst)
     if inst.components.container then
         inst.components.container:Close()
@@ -88,107 +87,56 @@ end
 AddPrefabPostInit("candybag", candybag_fn)
 
 ----------------------------------------------种子袋----------------------------------------------
-local function seedpouch_item_fn(container, item, slot)
-    return item.prefab == "seeds" or string.match(item.prefab or "", "_seeds") or item:HasTag("treeseed")
-end
+if GetModConfigData("seedpouch_portable") then
+    local function seedpouch_item_fn(container, item, slot)
+        return item.prefab == "seeds" or string.match(item.prefab or "", "_seeds") or item:HasTag("treeseed")
+    end
 
---种子袋属性
-local seedpouch_data = {
-    widget = {
-        slotpos = {},
-        animbank = "ui_krampusbag_2x8",
-        animbuild = "ui_krampusbag_2x8",
-        pos = Vector3(600, -120, 0)
-    },
-    issidewidget = false,
-    type = "portable_bag",
-    openlimit = 1
-}
+    --种子袋属性
+    local seedpouch_data = {
+        widget = {
+            slotpos = {},
+            animbank = "ui_krampusbag_2x8",
+            animbuild = "ui_krampusbag_2x8",
+            pos = Vector3(600, -120, 0)
+        },
+        issidewidget = false,
+        type = "portable_bag",
+        openlimit = 1
+    }
 
-for y = 0, 6 do
-    table.insert(seedpouch_data.widget.slotpos, Vector3(-162, -75 * y + 240, 0))
-    table.insert(seedpouch_data.widget.slotpos, Vector3(-162 + 75, -75 * y + 240, 0))
-end
+    for y = 0, 6 do
+        table.insert(seedpouch_data.widget.slotpos, Vector3(-162, -75 * y + 240, 0))
+        table.insert(seedpouch_data.widget.slotpos, Vector3(-162 + 75, -75 * y + 240, 0))
+    end
 
-seedpouch_data.itemtestfn = seedpouch_item_fn
-seedpouch_data.priorityfn = seedpouch_data.itemtestfn
+    seedpouch_data.itemtestfn = seedpouch_item_fn
+    seedpouch_data.priorityfn = seedpouch_data.itemtestfn
 
---种子袋修改逻辑
-local function seedpouch_fn(inst)
-    if TheNet:GetIsServer() then
-        if inst.components.container then
-            inst.components.container:WidgetSetup("seedpouch", seedpouch_data)
-            inst.components.container.onopenfn = onopen
-            inst.components.container.onclosefn = onclose
-        end
-
-        if inst.components.inventoryitem then
-            inst.components.inventoryitem.cangoincontainer = true
-            inst.components.inventoryitem:SetOnDroppedFn(ondropped)
-            if inst.components.equippable then
-                inst:RemoveComponent("equippable")
+    --种子袋修改逻辑
+    local function seedpouch_fn(inst)
+        if TheNet:GetIsServer() then
+            if inst.components.container then
+                inst.components.container:WidgetSetup("seedpouch", seedpouch_data)
+                inst.components.container.onopenfn = onopen
+                inst.components.container.onclosefn = onclose
             end
-        end
-    elseif TheNet:GetIsClient() then
-        inst.OnEntityReplicated = function(inst)
-            if inst.replica.container then
-                inst.replica.container:WidgetSetup("seedpouch", seedpouch_data)
+
+            if inst.components.inventoryitem then
+                inst.components.inventoryitem.cangoincontainer = true
+                inst.components.inventoryitem:SetOnDroppedFn(ondropped)
+                if inst.components.equippable then
+                    inst:RemoveComponent("equippable")
+                end
+            end
+        elseif TheNet:GetIsClient() then
+            inst.OnEntityReplicated = function(inst)
+                if inst.replica.container then
+                    inst.replica.container:WidgetSetup("seedpouch", seedpouch_data)
+                end
             end
         end
     end
+
+    AddPrefabPostInit("seedpouch", seedpouch_fn)
 end
-
-AddPrefabPostInit("seedpouch", seedpouch_fn)
-
-----------------------------------------------优先级----------------------------------------------
---参考能力勋章(优先入盒)
--- local function inventory_fn(self)
---     local oldGiveItem = self.GiveItem
---     self.GiveItem = function(self, inst, slot, src_pos)
---         if inst.components.inventoryitem == nil or not inst:IsValid() then
---             print("Warning: Can't give item because it's not an inventory item.")
---             return
---         end
-
---         --没有目标格子，才需找到格子
---         if not slot then
---             local portable_bag_name = nil
-
---             if candybag_item_fn(nil, inst, nil) then
---                 portable_bag_name = "candybag"
---             elseif seedpouch_item_fn(nil, inst, nil) then
---                 portable_bag_name = "seedpouch"
---             end
-
---             --符合某个模组容器条件，才找模组容器
---             if portable_bag_name then
---                 if inst.components.inventoryitem.owner and inst.components.inventoryitem.owner ~= self.inst then
---                     inst.components.inventoryitem:RemoveFromOwner(true)
---                 end
-
---                 local objectDestroyed = inst.components.inventoryitem:OnPickup(self.inst, src_pos)
---                 if objectDestroyed then
---                     return
---                 end
-
---                 --找到容器
---                 local portable_bag =
---                     self:FindItem(
---                     function(item)
---                         return item.prefab == portable_bag_name and item.components.container and
---                             item.components.container:IsOpen()
---                     end
---                 )
-
---                 local portable_bag_container = portable_bag and portable_bag.components.container
---                 if portable_bag_container and portable_bag_container:GiveItem(inst, nil, src_pos) then
---                     return true
---                 end
---             end
---         end
-
---         return oldGiveItem and oldGiveItem(self, inst, slot, src_pos)
---     end
--- end
-
--- AddComponentPostInit("inventory", inventory_fn)
